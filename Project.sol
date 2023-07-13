e7a56c2937eb900b939b21101cef060af0317acd
Satya prakash <136918622+Satya-1107@users.noreply.github.com>
2023-07-13 08:50:29 +0530

    Create Project.sol

---
+++ Project.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DegenToken is ERC20, Ownable {
+    constructor() ERC20("Degen", "DGN") {}
+
+    function mint(address recipient, uint256 amount) external onlyOwner {
+        _mint(recipient, amount);
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        return true;
+    }
+
+    function transferFrom(address from, address recipient, uint256 amount) public override returns (bool) {
+        _transfer(from, recipient, amount);
+        _approve(from, msg.sender, allowance(from, msg.sender) - amount);
+        return true;
+    }
+
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        _approve(msg.sender, spender, amount);
+        return true;
+    }
+
+    function redeem(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    function checkBalance(address account) external view returns (uint256) {
+        return balanceOf(account);
+    }
+
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+}
